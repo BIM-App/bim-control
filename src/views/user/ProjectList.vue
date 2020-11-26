7413b18a2709f32c297d455a9fc410a55bf18e9c
zakke27 <zakke27@foxmail.com>
2020-11-27 00:07:49 +0800

    add file-upload

--- src/views/user/ProjectList.vue
+++ src/views/user/ProjectList.vue
@@ -2,39 +2,48 @@
   <div id="projectlist">
     <!-- 功能栏 -->
     <div class="tool">
-      <el-button size="mini" type="primary" @click="dialogFormVisible = true">添加</el-button>
+      <el-button type="text" @click="dialogFormVisible = true">添加</el-button>
     </div>
+    <!-- 表单Dialog框 -->
+    <el-dialog title="新建项目" :visible.sync="dialogFormVisible">
+      <el-row :gutter="15">
+        <el-form ref="form" :model="form" label-width="100px">
+          <!-- 项目名称 -->
+          <el-col :span="15">
+            <el-form-item label="项目名称" prop="name">
+              <el-input v-model="form.name" autocomplete="off" prefix-icon='el-icon-mobile'>
+              </el-input>
+            </el-form-item>
+          </el-col>
+          <!-- 关联图片 -->
+          <el-col :span="24">
+            <el-form-item label="关联图片" prop="pictureFileURL" required>
+              <el-upload class="upload-pic" action="" :on-change="handleChange"
+                :http-request="allUpload" ref="upload" :auto-upload="false"
+                list-type="picture-card">
+                <i class="el-icon-plus"></i>
+                <div class="el-upload__tip" slot="tip">只能上传jpg/png文件</div>
+              </el-upload>
+            </el-form-item>
+          </el-col>
+        </el-form>
+      </el-row>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false;submitUpload()">确 定
+        </el-button>
+      </div>
+    </el-dialog>
     <!-- 动态渲染列表 -->
     <ul class="cards">
       <li class="card_item" v-for="(item, index) in lists" :key="index">
-        {{item.name}}
+        项目名称{{item.name}}
         <img :src="item.imgUrl" />
-
+        <el-button type="mini" @click="lists.splice(index,1);subProject()" :data-index="item.id">
+          删除 id{{item.id}}
+        </el-button>
       </li>
     </ul>
-    <!-- 表单框 -->
-    <el-dialog title="添加项目" :visible.sync="dialogFormVisible">
-      <el-form :model="form" ref="form">
-        <el-form-item label="项目名称" :label-width="formLabelWidth" prop="name">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="项目负责人" :label-width="formLabelWidth" prop="creatorUser">
-          <el-input v-model="form.creatorUser" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="上传图片" :label-width="formLabelWidth" prop="pictureFileUrl">
-          <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/"
-            multiple>
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-          </el-upload>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addProject (dialogFormVisible = false)">确 定</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
@@ -47,46 +56,49 @@ export default {
   data() {
     return {
       lists: [
-        { name: 'zakke', imgUrl: 'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxlo91kiwj31hc0u0k9u.jpg' },
-        { name: 'zhangsan', imgUrl: 'https://tva4.sinaimg.cn/large/87c01ec7gy1frmbm526o7j21hc0u0ajj.jpg' }
+        // { id: 1, name: 'zakke', imgUrl: 'https://tva2.sinaimg.cn/large/0072Vf1pgy1foxlo91kiwj31hc0u0k9u.jpg' },
+        // { id: 2, name: 'zhangsan', imgUrl: 'https://tva4.sinaimg.cn/large/87c01ec7gy1frmbm526o7j21hc0u0ajj.jpg' },
+        // {
+        // }
       ],
       dialogFormVisible: false,
       form: {
         name: '',
-        creatorUser: '',
-        pictureFileURL: ''
-      },
-      formLabelWidth: '120px'
+        pictureFile: '',
+      }
     }
   },
   created() {
-    console.log('created');
+    // console.log('created');
   },
   updated() {
-    console.log('updated');
+    // console.log('updated');
   },
   methods: {
-    addProject() {
-      const _this = this
-      this.$axios.post('http://47.112.132.91:8080', {
-        name: _this.form.name,
-        pictureFileURL: _this.form.pictureFileURL
-      }).then((result) => {
-        // print服务端返回的数据对象
-        console.log(result.data);
-        if (result.data.code == 1) {
-          this.$alert('项目创建成功', '提示', {
-            confirmButtonText: '确定',
-            callback: () => {
-              // _this.$refs.form.resetFields()
-            }
-          })
+    handleChange(file, fileList) {
+      this.form.pictureFile = file.raw
+      // console.log(this.form.pictureFile);
+    },
+    allUpload() {
+      let formData = new FormData()
+      formData.append('name', this.form.name);
+      formData.append('pictureFile', this.form.pictureFile)
+
+      let config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
         }
+      }
+      this.$axios.post('http://47.112.132.91:8080/Test/addPicture', formData, config).then((result) => {
+        console.log(result);
       }).catch((err) => {
         console.log(err);
       });
+    },
+    submitUpload(param) {
+      this.$refs.upload.submit();
     }
-  },
+  }
 }
 </script>
 
@@ -101,8 +113,12 @@ export default {
   width: 100%;
   height: 40px;
   line-height: 40px;
-  background-color: purple;
+  background-color: pink;
 }
+.el-upload__tip {
+  line-height: 1.2;
+}
+
 li {
   list-style: none;
 }
