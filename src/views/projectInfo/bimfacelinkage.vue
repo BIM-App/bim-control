0b65582c8ff23751e78943c479a53b4173879ed3
zakke27 <zakke27@foxmail.com>
2021-03-03 23:41:18 +0800

    3d marker

--- src/views/projectInfo/bimfacelinkage.vue
+++ src/views/projectInfo/bimfacelinkage.vue
@@ -27,6 +27,15 @@
     >
       {{ content }}
     </div>
+    <div class="buttons">
+      <button type="button" class="button" @click="add3DMarker()">
+        点击构件添加标签
+      </button>
+      <button id="btnClearItems" class="button" @click="clearItems()">
+        清除标签
+      </button>
+    </div>
+    <!-- 定义DOM元素，用于在该DOM元素中显示模型或图纸 -->
     <div id="domId" class="domId" />
     <div
       v-show="modelList.length != 0"
@@ -63,7 +72,9 @@ export default {
       contentFlag: false,
       content: '', // 错误描述
       loadModel: false,
-      modelToken: ''
+      modelToken: '',
+      marker: '',
+      isAddItemActtivated: false
     }
   },
   computed: {},
@@ -71,6 +82,7 @@ export default {
     console.log('creator', getAccessToken())
   },
   mounted() {
+    var _this = this
     this.loadModel = true
     const data = {
       fileId: this.modelFileId
@@ -110,7 +122,9 @@ export default {
         this.loadModel = false
         console.log(err)
       })
-
+    var options = new BimfaceSDKLoaderConfig()
+    options.viewToken = _this.viewToken
+    BimfaceSDKLoader.load(options, _this.successCallback, _this.failureCallback)
     // this.getToken()
     // this.init();
   },
@@ -127,142 +141,110 @@ export default {
       // eslint-disable-next-line no-unused-vars
       var viewAdded = false
       // 初始化显示组件
-      // eslint-disable-next-line no-undef
+      // eslint-disable-next-line no-undefw
       var options = new BimfaceSDKLoaderConfig()
       options.viewToken = this.modelToken
       // eslint-disable-next-line no-undef
-      BimfaceSDKLoader.load(options, successCallback, failureCallback)
-      function successCallback(viewMetaData) {
-        // eslint-disable-next-line eqeqeq
-        if (viewMetaData.viewType == '3DView') {
-          // ======== 判断是否为3D模型 ========
-          // 获取DOM元素
-          var dom4Show = document.getElementById('domId')
-          // eslint-disable-next-line no-undef
-          var webAppConfig = new Glodon.Bimface.Application.WebApplication3DConfig()
-          webAppConfig.domElement = dom4Show
-          // 创建WebApplication
-          if (!_this.app) {
-            // eslint-disable-next-line no-undef
-            _this.app = new Glodon.Bimface.Application.WebApplication3D(
-              webAppConfig
-            )
-          }
-          // 添加待显示的模型
-          _this.app.addView(_this.modelToken)
-          // 从WebApplication获取viewer3D对象
-          _this.viewer3D = _this.app.getViewer()
-          // 监听添加view完成的事件
-          _this.viewer3D.addEventListener(
-            // eslint-disable-next-line no-undef
-            Glodon.Bimface.Viewer.Viewer3DEvent.ViewAdded,
-            function() {
-              console.log(_this.fileName)
-              const aa = _this.fileName.split('.')
-              console.log(aa[aa.length - 1])
-              // eslint-disable-next-line eqeqeq
-              if (aa[aa.length - 1] == 'rvt') {
-                // eslint-disable-next-line no-undef
-                var mapConfig = Glodon.Bimface.Plugins.Map.MapConfig()
-                mapConfig.domElement = document.getElementById('map') // 页面的dom元素
-                mapConfig.viewer = _this.viewer3D // 三维模型
-                if (!_this.map) {
-                  // eslint-disable-next-line no-undef
-                  _this.map = new Glodon.Bimface.Plugins.Map.Map(mapConfig)
-                }
-                var l = _this.map.getFloorList() // 获取楼层的小地图列表
-                _this.modelList = l
-                _this.map.showFloorById(l[0].id) // 显示某一张小地图
-              }
-              // 自适应屏幕大小
-              window.onresize = function() {
-                _this.viewer3D.resize(
-                  document.documentElement.clientWidth,
-                  document.documentElement.clientHeight - 40
-                )
-              }
-              viewAdded = true
-              // 渲染3D模型
-              // let a = Glodon.Bimface.Viewer.NavigationMode3D.Walk;
-              // _this.viewer3D.setNavigationMode(a); //开启漫游
-              // _this.viewer3D.setFlySpeedRate(1); //漫游速度
-              _this.viewer3D.render()
+      BimfaceSDKLoader.load(options, _this.successCallback, _this.failureCallback)
+    },
+    // 加载成功回调函数
+    successCallback(viewMetaData) {
+      var _this = this
+      if (viewMetaData.viewType == '3DView') {
+        // 获取DOM元素
+        var WebAppConfig = new Glodon.Bimface.Application.WebApplication3DConfig()
+        WebAppConfig.domElement = document.getElementById('domId')
+        var app = new Glodon.Bimface.Application.WebApplication3D(WebAppConfig)
+        _this.viewer3D = app.getViewer()
+        app.addView(viewMetaData.viewToken)
+        // 三维标签的配置类
+        var markerConfig = new Glodon.Bimface.Plugins.Marker3D.Marker3DContainerConfig()
+        markerConfig.viewer = _this.viewer3D
+        _this.marker = new Glodon.Bimface.Plugins.Marker3D.Marker3DContainer(
+          markerConfig
+        )
+        _this.viewer3D.addEventListener(
+          Glodon.Bimface.Viewer.Viewer3DEvent.ViewAdded,
+          function() {
+            // 自适应屏幕大小
+            window.onresize = function() {
+              _this.viewer3D.resize(
+                document.documentElement.clientWidth,
+                document.documentElement.clientHeight - 40
+              )
             }
-          )
-        } else {
-          // 是图纸时的函数
-          const dom4Show = document.getElementById('domId')
-          // eslint-disable-next-line no-undef
-          const webAppConfig = new Glodon.Bimface.Application.WebApplicationDrawingConfig()
-          webAppConfig.domElement = dom4Show
-          // 创建WebApplication
-          if (!_this.app) {
-            // eslint-disable-next-line no-undef
-            _this.app = new Glodon.Bimface.Application.WebApplicationDrawing(
-              webAppConfig
-            )
           }
-          // 添加待显示图纸
-          _this.app.load(_this.modelToken)
-          // 从WebApplication获取viewer2D对象
-          _this.viewer3D = _this.app.getViewer()
-          _this.viewer3D.addEventListener(
-            // eslint-disable-next-line no-undef
-            Glodon.Bimface.Viewer.ViewerDrawingEvent.Loaded,
-            function(data) {
-              window.onresize = function() {
-                _this.viewer3D.resize(
-                  document.documentElement.clientWidth,
-                  document.documentElement.clientHeight - 40
-                )
-              }
-            }
-          )
+        )
+      }
+    },
+    // 加载失败回调函数
+    failureCallback(error) {
+      console.log(error)
+    },
+    // 增加三维标签
+    addItems(objectdata) {
+      var _this = this
+      var position = objectdata.worldPosition
+      var marker3dConfig = new Glodon.Bimface.Plugins.Marker3D.Marker3DConfig()
+      marker3dConfig.src =
+        'http://static.bimface.com/resources/3DMarker/warner/warner_red.png'
+      marker3dConfig.worldPosition = position
+      // 三维标签的提示
+      marker3dConfig.tooltip = 'this is 3DMarker.'
+      var marker3d = new Glodon.Bimface.Plugins.Marker3D.Marker3D(
+        marker3dConfig
+      )
+      marker3d.onClick(function(item) {
+        // 获取点击图片的postion
+        var m = item.position
+        // 自己设置一个boundingbox的对象
+        var num = 1.1
+        var max = m.x * num
+        var may = m.y * num
+        var maz = m.z * num
+        var mix = m.x / num
+        var miy = m.y / num
+        var miz = m.z / num
+        var maxp = new Object()
+        maxp = {
+          x: max,
+          y: may,
+          z: maz
         }
+        var minp = new Object()
+        minp = {
+          x: mix,
+          y: miy,
+          z: miz
+        }
+        var boundingbox = new Object()
+        boundingbox = {
+          max: maxp,
+          min: minp
+        }
+        // 缩放到该boundingbox
+        _this.viewer3D.zoomToBoundingBox(boundingbox)
+      })
+      _this.marker.addItem(marker3d)
+      _this.viewer3D.render()
+    },
+    add3DMarker() {
+      var _this = this
+      if (_this.isAddItemActtivated) {
+        return
+      } else {
+        // 添加点击监听事件
+        _this.viewer3D.addEventListener(
+          Glodon.Bimface.Viewer.Viewer3DEvent.MouseClicked,
+          _this.addItems
+        )
+        _this.isAddItemActtivated = true
       }
-      function failureCallback(error) {
-        console.log(error)
-      }
+    },
+    clearItems() {
+      const _this = this
+      _this.marker.clear()
     }
-    // getToken() {
-    //   this.loadModel = true
-    //   getmodeltokenApi({
-    //     data: {
-    //       id: this.MFile,
-    //       IsIntegrateId: this.isIntegrate,
-    //       accessToken: getAccessToken()
-    //     }
-    //   })
-    //     .then((res) => {
-    //       this.loadModel = false
-    //       const data = JSON.parse(res)
-    //       if (data.data) {
-    //         this.modelToken = data.data
-    //         this.init()
-    //       } else {
-    //         this.contentFlag = true
-    //       }
-    //       if (data.code === 'authentication.failed') {
-    //         this.content = 'BIMFACE系统异常,请联系管理员'
-    //       }
-    //       if (data.code === 'system.error') {
-    //         this.content = 'API访问合法性校验失败,请联系管理员'
-    //       }
-    //       if (data.code === 'file.has.not.translated') {
-    //         this.content = '文件未转换,请联系管理员'
-    //       }
-    //       if (data.code === 'file.translate.failed') {
-    //         this.content = '文件转换失败,请联系管理员'
-    //       }
-    //       if (data.code === 'file.is.translating') {
-    //         this.content = '文件转换中,请稍后'
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       this.loadModel = false
-    //       console.log(err)
-    //     })
-    // }
   }
 }
 </script>
